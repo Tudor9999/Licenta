fa63902defbe7517cd03f0eadccb00588c735bbb
Tudor Toma <tudortoma2509@gmail.com>
2024-06-04 00:28:33 +0300

    Add attack animation and changing to animation tree

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 175.0
+const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+@export var attacking = false
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -18,7 +20,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	
@@ -28,13 +29,16 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	
 	#Play animations
+	
 	if is_on_floor():
+		
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+	
 	
 	
 	
