b24c3af8cb5d6cad2c445021d504cc1a2787811f
Tudor9999 <tudortoma2509@gmail.com>
2024-06-04 13:49:49 +0300

    Update player animation and control + add player death animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,9 +7,10 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
-@export var attacking = false
+@onready var sprite_2d = $Sprite2D
+
 
 func _physics_process(delta):
 	
@@ -24,20 +25,22 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction > 0:
-		animated_sprite.flip_h = false
+		sprite_2d.flip_h = false
+		sprite_2d.offset = Vector2(0, 0)
 	elif direction < 0:
-		animated_sprite.flip_h = true
+		sprite_2d.offset = Vector2(-41, 0)
+		sprite_2d.flip_h = true
 	
 	#Play animations
 	
 	if is_on_floor():
 		
 		if direction == 0:
-			animated_sprite.play("idle")
+			animation_player.play("idle")
 		else:
-			animated_sprite.play("run")
+			animation_player.play("run")
 	else:
-		animated_sprite.play("jump")
+		animation_player.play("jump")
 	
 	
 	
